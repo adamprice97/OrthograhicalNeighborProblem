21bae3ace0057c6b161ecab253c2aadb466a7c00
AdamPrice97 <twocolossi@gmail.com>
2020-09-08 13:37:35 +0100

    Add project files.

---
+++ OrthograhicalNeighborProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrthograhicalNeighborProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
